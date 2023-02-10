ce85e5a53bda3d953486946d1404ff7439ca5c53
MorsyB <morsyb.6c@gmail.com>
2023-02-10 17:27:33 +0200

    UPDATE MAIN

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import urllib.request
 import cv2
-import time
+from djitellopy import Tello
 import numpy as np
+import time
 
+drone = Tello()
+drone.connect()
+print(drone.get_battery())
+drone.takeoff()
+
+center_offset = 80
+takeoff_flag = False
 
 def get_img():
     images = urllib.request.urlopen("http://192.168.1.175:8080/shot.jpg")
@@ -11,30 +19,43 @@ def get_img():
 
 
 def find_path(window_center, image_center):
-    if window_center[0] - image_center[0] > 50:
+    if window_center[0] - image_center[0] > center_offset:
         print("GO RIGHT")
+        drone.move_right(20)
+        time.sleep(2)
+    elif window_center[0] - image_center[0] < -center_offset:
+        print("GO LEFT")
+        drone.move_left(20)
+        time.sleep(2)
+    elif window_center[1] - image_center[1] > center_offset:
+        print("GO DOWN")
+        drone.move_down(20)
+        time.sleep(2)
+    elif window_center[1] - image_center[1] < -center_offset:
+        print("GO UP")
+        drone.move_up(20)
+        time.sleep(2)
     else:
-        if window_center[0] - image_center[0] < -50:
-            print("GO LEFT")
-        else:
-            if window_center[1] - image_center[1] > 50:
-                print("GO Down")
-            else:
-                if window_center[1] - image_center[1] < -50:
-                    print("GO UP")
-                else:
-                    print("GO FORWARD")
+        print("GO FORWARD")
 
 
 def navigate():
     # cap = cv2.VideoCapture('video4.mp4')
     frames = []
+    drone.streamon()
+    #drone.takeoff()  # takeoff command
+    time.sleep(5)
+    drone.move_up(20)
+    time.sleep(2)
     while True:
         # Read a frame
         # _, frame = cap.read()
 
         # Get the frame from the stream
-        image = get_img()
+        image = drone.get_frame_read().frame
+        while image is None:
+            image = drone.get_frame_read().frame
+        time.sleep(0.1)
 
         # the center of the frame
         height, width = image.shape[:2]
@@ -72,12 +93,29 @@ def navigate():
 
             # Show the image
             cv2.imshow("Frame", image)
-
             # Exit if the user presses the "q" key
             if cv2.waitKey(1) == ord("q"):
                 break
             frames.append(image)
+            time.sleep(0.1)
+
+
+def new_Algo():
+    drone.takeoff()
+    drone.streamon()
+    time.sleep(5)
+    while True:
+        image = drone.get_frame_read().frame
+        cv2.imshow("Frame", image)
+        cv2.waitKey(1)
+        time.sleep(0.1)
+
+    print("sleep is finished like cr7")
+    drone.land()
+    drone.streamoff()
+    print("lsnding")
 
 
 if __name__ == '__main__':
+    #new_Algo()
     navigate()
